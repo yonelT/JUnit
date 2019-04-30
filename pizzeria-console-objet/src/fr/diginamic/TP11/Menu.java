9f7f64f898cf42f56c833790472bb48d1b146c54
Yonel TOUSSAY <yonel.toussay@gmail.com>
2019-04-30 17:39:17 +0200

    TP 11.2

--- pizzeria-console-objet/src/fr/diginamic/TP11/Menu.java
+++ pizzeria-console-objet/src/fr/diginamic/TP11/Menu.java
@@ -1,16 +1,16 @@
 package fr.diginamic.TP11;
 
 public class Menu {
-	
+
 	public Menu() {
-		
+
 	}
-	
+
 	public void afficher() {
-		System.out.println("***** Pizzeria Administration *****");
+		System.out.println("\n***** Pizzeria Administration *****");
 		System.out.println("1. Lister les pizzas");
 		System.out.println("2. Ajouter une nouvelle pizza");
-		System.out.println("3. Mettre à jour une pizza");
+		System.out.println("3. Mettre Ã  jour une pizza");
 		System.out.println("4. Supprimer une pizza");
 		System.out.println("99. Sortir");
 		System.out.println("\nFaite un choix");
--- pizzeria-console-objet/src/fr/diginamic/TP11/PizzeriaAdminConsoleApp.java
+++ pizzeria-console-objet/src/fr/diginamic/TP11/PizzeriaAdminConsoleApp.java
@@ -8,48 +8,62 @@ import java.util.Scanner;
 public class PizzeriaAdminConsoleApp {
 
 	public static void main(String[] args) {
+		List<Pizza> listePizza = new ArrayList<Pizza>();
 		Menu monMenu = new Menu();
 		Scanner sc = new Scanner(System.in);
-		int choix = 0;
-		Pizza maPizza = new Pizza(1,"MARG","Margharita",11.99); //arraylist de pizza?
-		Pizza maPizza2 = new Pizza(2,"VEG","Vegetarienne",13.99);
-		List<Pizza> liste = new ArrayList<Pizza>();
-		
+		String choix = null;
+
+		listePizza.add(new Pizza(0, "PEP", "Pépéroni", 12.50));
+		listePizza.add(new Pizza(1, "MAR", "Margherita", 12.50));
+		listePizza.add(new Pizza(2, "REIN", "La Reine", 11.50));
+		listePizza.add(new Pizza(3, "FRO", "La 4 fromages", 12.00));
+		listePizza.add(new Pizza(4, "CAN", "La cannibale", 12.50));
+		listePizza.add(new Pizza(5, "SAV", "La savoyarde", 13.00));
+		listePizza.add(new Pizza(6, "ORI", "L’orientale", 13.50));
+		listePizza.add(new Pizza(7, "IND", "L’indienne", 14.00));
+
 		monMenu.afficher();
-		choix = sc.nextInt();
-		//A modif : boucle while � rajouter
+		choix = sc.nextLine();
+
+		while (!choix.equals("1") && !choix.equals("2") && !choix.equals("3") && !choix.equals("4")
+				&& !choix.equals("99")) {
+			System.out.println("Choix inexistant!\nFaite un choix valide");
+			choix = sc.nextLine();
+		}
+
 		switch (choix) {
-		
-		case 1:
+
+		case "1":
 			System.out.println("Liste des pizzas");
-			liste.add(maPizza); //a modif
-			liste.add(maPizza2); //a modif
-			for (Pizza piz : liste) {
+
+			for (Pizza piz : listePizza) {
 				System.out.println(piz);
 			}
+
+			// Iterator<Pizza> iter = new Iterator<Pizza>();
 			break;
-		case 2:
-			System.out.println("Ajout d�une nouvelle pizza");
-			liste.add(maPizza);
-			liste.add(maPizza2);
-			//maPizza.ajouter(new Pizza(1,"MARG","Margharita",11.99)); a modif
+		case "2":
+			System.out.println("Ajout d'une nouvelle pizza");
+			// listePizza.add(new Pizza(0, "PEP", "Pépéroni", 12.50));
+			// listePizza.add(new Pizza(1, "MAR", "Margherita", 12.50));
+
 			break;
-		case 3:
-			System.out.println("Mise � jour d'une pizza");
+		case "3":
+			System.out.println("Mise à jour d'une pizza");
 			break;
-		case 4:
+		case "4":
 			System.out.println("Suppression d'une pizza");
 			break;
-		case 99:
+		case "99":
 			System.out.println("Au revoir :(");
 			break;
-		
+
 		default:
 			System.out.println("Choix inexistant!");
 			break;
 		}
 		monMenu.afficher();
-		
+
 		sc.close();
 	}
 
