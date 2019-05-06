f6f39b78818a157bc5e2e2e05ba93d5177977429
yonel toussay <yonel.toussay@gmail.com>
2019-05-06 16:55:06 +0200

    jUnit

---
+++ demo-test-unitaires/src/main/java/fr/diginamic/demo_test_unitaires/App.java
@@ -0,0 +1,13 @@
+package fr.diginamic.demo_test_unitaires;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
